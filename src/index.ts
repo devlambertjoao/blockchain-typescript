3c75bb4593f9529094ece28255d07ebab1d6834e
Joao Lambert <joao.lambert@gft.com>
2022-02-04 12:50:28 -0300

    Mining and mining difficulty

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,33 @@ class Block {
   previousHash: string;
   hash: string;
   data: Transaction;
+  nonce: number;
 
   constructor(data: Transaction, previousHash: string) {
     this.createdDate = new Date(2022, 2, 2);
     this.data = data;
     this.previousHash = previousHash;
+    this.nonce = 0;
     this.hash = this.getHash();
   }
 
   private getHash = (): string => SHA256(JSON.stringify(this)).toString();
+
+  mineBlock(difficulty: number) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.getHash();
+    }
+
+    console.log(`Block mined ${this.hash}`);
+  }
 }
 
 class Blockchain {
   private chain: Array<Block>;
+  private difficulty: number = 2;
 
   constructor() {
     this.chain = [];
@@ -39,6 +53,7 @@ class Blockchain {
   getLatestBlockHash = (): string => this.getLatestBlock().hash;
 
   addBlock(block: Block) {
+    block.mineBlock(this.difficulty);
     this.chain.push(block);
   }
 }
@@ -50,6 +65,16 @@ lasanhaCoin.addBlock(
       from: "nemo",
       to: "crazyBoy",
       value: 200,
+    },
+    lasanhaCoin.getLatestBlockHash()
+  )
+);
+lasanhaCoin.addBlock(
+  new Block(
+    {
+      from: "nemo",
+      to: "TULIO",
+      value: 10,
     },
     lasanhaCoin.getLatestBlockHash()
   )
